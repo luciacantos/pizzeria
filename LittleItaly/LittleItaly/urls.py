074ffb050d23a6b072b65031633370967559f196
luciacantos <luciacantos22@gmail.com>
2024-12-25 17:40:51 +0100

    vista para pagina de inicio

--- LittleItaly/LittleItaly/urls.py
+++ LittleItaly/LittleItaly/urls.py
@@ -14,10 +14,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path, include
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('usuarios/', include('usuarios.urls')),
+    path('usuarios/', include('usuarios.urls')),  # Rutas de usuarios
+    path('', views.home, name='home'),  # Página principal
 ]
