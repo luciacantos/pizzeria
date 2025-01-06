843dda1c1b5bb8b269ac23f2577cecaa5cc1b366
luciacantos <luciacantos22@gmail.com>
2025-01-06 17:29:56 +0100

    conexión API EDAMAM

--- LittleItaly/menu/models.py
+++ LittleItaly/menu/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Recipe(models.Model):
+    name = models.CharField(max_length=255)
+    image = models.URLField()
+    healthLabels = models.TextField()
+    cuisineType = models.TextField()
+    calories = models.FloatField()
+    totalNutrients = models.JSONField()
+    ingredients = models.TextField()
+    def __str__(self):
+        return self.name
