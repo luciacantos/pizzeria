9ebab4a9346fba70c5d111ddc4215f92ede4ab4e
Jacobo24 <114859002+Jacobo24@users.noreply.github.com>
2025-01-07 17:59:48 +0100

    conectar correctamente los archivos

--- LittleItaly/LittleItaly/views.py
+++ LittleItaly/LittleItaly/views.py
@@ -2,13 +2,4 @@ import requests
 from django.shortcuts import render
 
 def home(request):
-    # Llamada a la API de pizzas
-    api_url = "https://api.pizzas.com/v1/pizzas"
-    response = requests.get(api_url)
-    
-    if response.status_code == 200:
-        api_data = response.json()  # Datos obtenidos de la API
-    else:
-        api_data = {"error": "No se pudo conectar con la API"}
-
-    return render(request, "LittleItaly/home.html", {"api_data": api_data})
+    return render(request, 'LittleItaly/home.html')
